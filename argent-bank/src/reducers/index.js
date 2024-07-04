8c7ed35dd4142fa4964100cd5db2d021c0283a3a
Ghislain <ghislain.jambert@gmail.com>
2024-07-04 22:31:03 +0200

    mise à jour du projet + correction du fichier swagger

--- argent-bank/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-
-// Combinaison de tous les reducers en un seul reducer principal
-const rootReducer = combineReducers({
-  auth: authReducer,
-});
-
-export default rootReducer;
